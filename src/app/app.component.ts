c7d4832ce81e16412a5c689e66c1e27b59ac7290
chikuchalke <chalkevivek19@gmail.com>
2023-11-19 12:59:15 +0530

    add all layout changes and img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component ,HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'deadstock';
+
+  sidebarActive : boolean = false;
+  backTopActive : boolean = false;
+  show : boolean = false;
+  whatsAppactive : boolean = false;
+  showButton = false;
+
+  feedBackToggle(){
+    this.show = !this.show
+  }
+  whatsAppToggle(){
+    this.whatsAppactive = !this.whatsAppactive
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // Check the scroll position and toggle the button visibility
+    // this.showButton = window.pageYOffset > 400;
+    this.backTopActive = window.pageYOffset > 400;
+  }
+
+  scrollToTop() {
+    // Scroll to the top of the page
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  toggleSidebar(){
+    // this.sidebarActive = !this.sidebarActive
+    this.sidebarActive = true
+  }
+
+  closeSidebar(){
+    this.sidebarActive = false
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  @Input() isActive: boolean = false;
 
+  // @Input() isActiveLi: boolean = false;
+
+  @Output() cancelClicked = new EventEmitter<void>();
+
+  activeItem: number | null = null;
+
+  setActive(itemNumber: number) {
+    if (this.activeItem === itemNumber) {
+      this.activeItem = null; // Clicking the active item again removes the active class
+    } else {
+      this.activeItem = itemNumber;
+    }
+  }
+
+  onCancelClick() {
+    this.cancelClicked.emit();
+  }
 }
