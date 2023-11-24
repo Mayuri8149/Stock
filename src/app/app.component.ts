821ce00ba0e3e7464fd6781d0c5d5d660dd04525
Mayuri8149 <kate.mayuri14792@gmail.com>
2023-11-24 15:32:51 +0530

    Product detail component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component ,HostListener} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,39 +7,6 @@ import { Component ,HostListener} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'deadstock';
-
-  sidebarActive : boolean = false;
-  backTopActive : boolean = false;
-  show : boolean = false;
-  whatsAppactive : boolean = false;
-  showButton = false;
-
-  feedBackToggle(){
-    this.show = !this.show
-  }
-  whatsAppToggle(){
-    this.whatsAppactive = !this.whatsAppactive
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    // Check the scroll position and toggle the button visibility
-    // this.showButton = window.pageYOffset > 400;
-    this.backTopActive = window.pageYOffset > 400;
-  }
-
-  scrollToTop() {
-    // Scroll to the top of the page
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
-
-  toggleSidebar(){
-    // this.sidebarActive = !this.sidebarActive
-    this.sidebarActive = true
-  }
-
-  closeSidebar(){
-    this.sidebarActive = false
-  }
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[AppRoutingModule,RouterModule]
 })
 export class AppModule { }
